e9b017e14e49ece62fd640b03ab87be1ab579ef1
Josue Narro <josue.narro.a@gmail.com>
2020-07-05 17:55:45 -0500

    Add fetchItems function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header'
 import './App.css';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      console.log(result.data);
+    
+      setItems(result.data);
+      setIsLoading(false);
+    }
+
+    fetchItems();
+  }, [])
+
   return (
     <div className="container">
-      Hola
+      <Header />
     </div>
   );
 }
