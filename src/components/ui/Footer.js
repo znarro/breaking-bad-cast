89cbe58b6fe51c75181ef645921025d671cabd57
Josue Narro <josue.narro.a@gmail.com>
2020-07-06 00:00:52 -0500

    Fix footer typo

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='center'>
       <p className='madeby'>Made with <span role='img' aria-label='crystal emoji'>&#x1F48E;</span> by deveLawyer</p>
-      <p className=''>Inspired by &copy;TraversyMedia project</p>
+      <p className=''>Inspired by a &copy;TraversyMedia project</p>
     </footer>
   )
 }
