964c17c6490b5d849924137a4f283f215d5cf4ed
Josue Narro <josue.narro.a@gmail.com>
2020-07-05 20:27:30 -0500

    Search characters functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 import './App.css';
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);  // Characters' list state
+  const [isLoading, setIsLoading] = useState(true);  // Loading state for fetch characters
+  const [query, setQuery] = useState('');  // Input query state
 
   useEffect(() => {
+    // useEffect's callback can't be async, so...
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
       console.log(result.data);
-    
+
       setItems(result.data);
       setIsLoading(false);
     }
 
     fetchItems();
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(query) => setQuery(query)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
---
+++ src/components/ui/Search.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState('');
+
+  const setQuery = (query) => {
+    setText(query);
+    getQuery(query);
+  }
+
+  return (
+    <section className='search'>
+      <form>
+        <input 
+          type='text' 
+          className='form-control' 
+          placeholder='Search for your character...'
+          value={text}
+          onChange={(e) => setQuery(e.target.value)}
+          autoFocus 
+        />
+      </form>
+    </section>
+  )
+}
+
+export default Search
