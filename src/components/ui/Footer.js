842ac2a6b87ac23b0efcfff829357bb014c56baf
Josue Narro <josue.narro.a@gmail.com>
2020-07-26 04:05:28 -0500

    Update README.md

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer className='center'>
       <p className='madeby'>Made with <span role='img' aria-label='crystal emoji'>&#x1F48E;</span> by deveLawyer</p>
-      <p className=''>Inspired by a &copy;TraversyMedia project</p>
     </footer>
   )
 }
