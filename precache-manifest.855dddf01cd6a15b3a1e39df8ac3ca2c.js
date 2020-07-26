a120b35c068a00e8b27f8bf1d53617b9dda72a2a
Josue Narro <josue.narro.a@gmail.com>
2020-07-26 04:29:20 -0500

    Updates

---
+++ precache-manifest.855dddf01cd6a15b3a1e39df8ac3ca2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900d5e8f97c6b75cc66635da6b492b56",
+    "url": "/breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "01934a7ce3984a4a76cb",
+    "url": "/breaking-bad-cast/static/css/main.547c8071.chunk.css"
+  },
+  {
+    "revision": "cc7393bf9bda9a3b7340",
+    "url": "/breaking-bad-cast/static/js/2.417f8590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cast/static/js/2.417f8590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01934a7ce3984a4a76cb",
+    "url": "/breaking-bad-cast/static/js/main.334eaa00.chunk.js"
+  },
+  {
+    "revision": "aceb973de7feae88a752",
+    "url": "/breaking-bad-cast/static/js/runtime-main.039fb2eb.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
